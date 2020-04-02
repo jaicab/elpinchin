711c5382603c0eca85b424351fb919abfc9b84c7
Jaime Caballero <me@jaicab.com>
2020-04-02 19:09:41 +0200

    Fix responsiveness and remove slideshow

--- lib/header.php
+++ lib/header.php
@@ -1,10 +1,6 @@
 
 <div id="header">
-	<!-- jQuery handles to place the header background images -->
-	<div id="headerimgs">
-		<div id="headerimg1" class="headerimg"></div>
-		<div id="headerimg2" class="headerimg"></div>
-	</div>
+
 	<!-- Top navigation on top of the images -->
 	<div id="nav-outer">
 		<div id="navigation" class="content">
@@ -14,12 +10,12 @@
 			<?php include('menu.php'); ?>
 		</div>
 	</div>
-	<!-- Slideshow controls -->
-	<div id="headernav-outer">
-		<div id="headernav">
-			<div id="back" class="btn"></div>
-			<div id="control" class="btn"></div>
-			<div id="next" class="btn"></div>
-		</div>
+
+	<div id="headerimg">
+		<img
+			src="./images/familia.jpg"
+			alt="Paco y Maria en la barra de El
+		Pinchin"
+		/>
 	</div>
 </div>
--- lib/top.php
+++ lib/top.php
@@ -9,20 +9,5 @@
 
 <link href="/layout.css" type="text/css" rel="stylesheet" media="all" />
 <meta name="viewport" content="width=device-width; initial-scale=1.0" />
-<script type="text/javascript">
-addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-function hideURLbar(){ window.scrollTo(0,1);}
-</script>
 
-<script type="text/javascript" language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Lobster+Two:400' rel='stylesheet' type='text/css'>
-<!-- Slider -->
-<script type="text/javascript" src="lib/script.js"></script>
-
-<script language="javascript" type="text/javascript">
-$(function() {
-	$('#up').click(function(){
-		$('html, body').animate({scrollTop:0},'slow');
-	});
-});
-</script>
